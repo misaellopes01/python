c2cf3db9c73bcfe33fe5e77752989eb2388fc753
Misael Lopes <mecl.ely@gmail.com>
2023-07-24 14:17:30 +0100

    lesson 05

--- easystack.py
+++ easystack.py
@@ -4,66 +4,58 @@ def main():
     t = int(sys.stdin.readline())
     myStack = []
     readings(t, myStack)
-
+# usar uma unica pilha
 def readings(testCases, myStack):
-    iterationStack = []
-    printStack = []
-    for f in range(testCases):
-        iterationStack.append(sys.stdin.readline().split())
-        numbers = iterationStack[f]
-        if numbers[0] == "2" and len(myStack) > 0:
+    while testCases >= 1:
+        numbers = sys.stdin.readline().split()
+        if numbers[0] == "2" and myStack:
             myStack.pop()
-        if numbers[0] == "3":
-            length = len(myStack)
-            if length == 0:
-                printStack.append("Empty!")
+        elif numbers[0] == "3":
+            if not myStack:
+                sys.stdout.write("Empty!\n")
             else:
-                printStack.append(myStack[length - 1])
-        if numbers[0] == "1":
+                sys.stdout.write(f"{myStack[-1]}\n")
+        else:
+        # elif numbers[0] == "1":
             myStack.append(numbers[1])
-    showResult(printStack)
-
-def showResult(toPrint):
-    for i in range(len(toPrint)):
-        sys.stdout.write(f"{toPrint[i]}\n")
-
+        testCases -= 1
 main()
 # =============================================================================================
-import sys
-from io import StringIO
+# import sys
+# from io import StringIO
 
-def main():
-    t = int(input())
-    myStack = []
-    output_buffer = StringIO()
+# def main():
+#     t = int(input())
+#     myStack = []
+#     output_buffer = StringIO()
 
-    for _ in range(t):
-        numbers = input().split()
-        if numbers[0] == "2" and myStack:
-            myStack.pop()
-        elif numbers[0] == "3":
-            output_buffer.write(f"{myStack[-1]}\n" if myStack else "Empty!\n")
-        elif numbers[0] == "1":
-            myStack.append(numbers[1])
+#     for _ in range(t):
+#         numbers = input().split()
+#         if numbers[0] == "2" and myStack:
+#             myStack.pop()
+#         elif numbers[0] == "3":
+#             output_buffer.write(f"{myStack[-1]}\n" if myStack else "Empty!\n")
+#         elif numbers[0] == "1":
+#             myStack.append(numbers[1])
 
-    sys.stdout.write(output_buffer.getvalue())
+#     sys.stdout.write(output_buffer.getvalue())
 
-main()
-# ===========================================================================================
-import sys
-from collections import deque
+# main()
+# # ===========================================================================================
+# import sys
+# from collections import deque
 
-def main():
-    t = sys.stdin.readline()
-    myStack = deque()
+# def main():
+#     t = sys.stdin.readline()
+#     myStack = deque()
 
-    for _ in range(int(t)):
-        numbers = input().split()
-        if numbers[0] == "2" and myStack:
-            myStack.pop()
-        elif numbers[0] == "3":
-            sys.stdout.write(f"{myStack[-1]}\n" if myStack else "Empty!\n")
-        elif numbers[0] == "1":
-            myStack.append(numbers[1])
+#     for _ in range(int(t)):
+#         numbers = input().split()
+#         if numbers[0] == "2" and myStack:
+#             myStack.pop()
+#         elif numbers[0] == "3":
+#             sys.stdout.write(f"{myStack[-1]}\n" if myStack else "Empty!\n")
+#         elif numbers[0] == "1":
+#             myStack.append(numbers[1])
 
-main()
+# main()
