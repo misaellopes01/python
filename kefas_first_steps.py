38a0e4f756bf16afef418d1af08949c10e72ae3c
Misael Lopes <mecl.ely@gmail.com>
2023-08-03 15:35:04 +0100

    Create kefas_first_steps.py

---
+++ kefas_first_steps.py
@@ -0,0 +1,41 @@
+import sys
+
+def main():
+    n = int(sys.stdin.readline())
+    number = sys.stdin.readline().split(' ')
+    result = 0
+    count = 1
+    for i in range(1, n):
+        if number[i] >= number[i - 1]:
+            count += 1
+        else:
+            result = max(result, count)
+            count = 1
+
+    result = max(result, count)
+    print(result)
+       
+main()
+
+
+# import sys
+
+# def main():
+#     n = int(sys.stdin.readline())
+#     number = sys.stdin.readline().split(' ')
+#     result = []
+#     count = 1
+#     for i in range(n):
+#         position = i
+#         while position < len(number) - 1:
+#           if number[position] <= number[position + 1]:
+#             count += 1
+#             position += 1
+#           else:
+#               break
+#         if count > 0: result.append(count)
+#         count = 1
+#     sorted_result = sorted(result)
+#     print(sorted_result[len(result) - 1])
+       
+# main()
